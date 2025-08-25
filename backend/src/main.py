44eecd1638229e99a37a2543c0c14364f4e0816f
Fraimer De La Cruz <77998859+fraimerr@users.noreply.github.com>
2025-08-25 06:21:03 -0400

    ahaha

--- backend/src/main.py
+++ backend/src/main.py
@@ -161,7 +161,7 @@ async def history(session_id: str):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="127.0.0.1", port=5000)
+    uvicorn.run(app, host="127.0.0.1", port=6000)
 
     while True:
         query = input("Query: ")
